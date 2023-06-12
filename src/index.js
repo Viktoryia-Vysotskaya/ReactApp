55bedcba5dd69f7eed63e4b7ac9490a3c102d26d
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-12 22:21:54 +0200

    Update to React 18 / Reusable components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+createRoot(document.querySelector('#root')).render(<App />);
