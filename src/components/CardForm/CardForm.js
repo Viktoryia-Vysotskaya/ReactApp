b24cfc2929fc5bce8810b315cca3fd669d40fec0
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-15 11:29:04 +0200

    Update type of Button in CardForm

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -14,7 +14,7 @@ const CardForm = props => {
     return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
-            <Button type={props.type || "button"}>Add card</Button>        
+            <Button type="submit">Add card</Button>        
         </form>
     );
 };
