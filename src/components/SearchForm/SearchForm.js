554eb7a434eb345d2bfddadcb4311e4377abba49
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-24 08:55:21 +0200

    Add action selectors and wizards

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,17 +3,18 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
     const [phrase, setPhrase] = useState('');
     const dispatch = useDispatch();
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: phrase })
+        dispatch(updateSearchString(phrase))
     };
     return (
         <form className={styles.searchForm} onSubmit={handleSubmit}>
-            <TextInput id="searchPhrase" name="searchPhrase" placeholder="Search..." value={phrase} onChange={e => setPhrase(e.target.value)}/>
+            <TextInput id="searchPhrase" name="searchPhrase" placeholder="Search..." value={phrase} onChange={e => setPhrase(e.target.value)} />
             <Button type="submit">
                 <span className="fa fa-search" />
             </Button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import initialState from './initialState';
 import shortid from 'shortid';
 import { ADD_COLUMN, ADD_CARD, UPDATE_SEARCHSTRING } from './actionTypes';
+import { strContains } from '../utils/strContains';
+
+//selectors
+export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+export const getAllColumns = state => state.columns;
+// action creators
+export const addColumn = payload => ({ type: ADD_COLUMN, payload });
+export const addCard = payload => ({type: ADD_CARD, payload });
+export const updateSearchString = payload => ({type: UPDATE_SEARCHSTRING, payload}); 
 
 const reducer = (state, action) => {
   switch (action.type) {
