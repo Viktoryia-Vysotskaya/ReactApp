b97a0453f3a772c10ca6d89ffe6a16a4aa4c052d
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-27 21:29:30 +0200

    Add NavBar

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,7 +1,8 @@
 import styles from './Container.module.scss';
 
 const Container = props => {
-    return(<div className={styles.container}>{props.children}</div>)
+    return (
+        <div className={`${styles.container} ${props.className === undefined ? '' : props.className}`}>{props.children}</div>)
 }
 
 export default Container;
