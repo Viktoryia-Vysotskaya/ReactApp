a6c68d86e13827e918f89af49f89e65c09e5f77b
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-06-28 09:50:45 +0200

    Active Links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import Container from '../Container/Container';
 import styles from './NavBar.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -9,15 +10,12 @@ const NavBar = () => {
                     <i className={'fa fa-tasks'}></i>
                 </a>
                 <ul className={styles.navlinks}>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/favorite">Favorite</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/">Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/favorite">Favorite</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/about">About</NavLink></li>
                 </ul>
             </Container>
         </nav>
